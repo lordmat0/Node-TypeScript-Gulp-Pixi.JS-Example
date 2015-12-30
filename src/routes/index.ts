9afda435497bd1641f6e31936fb5dbd3d785efbb
matthew <lordmat@gmail.com>
2015-12-30 22:18:10 +0000

    Changed to using HTML

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,15 +1,17 @@
 import {Router} from 'express';
+import {join} from 'path';
 
 const index = Router();
 
 /* GET home page. */
 index.get('/', function(req, res, next) {
-  res.render('index', { title: 'Visual Studio Code!' });
+    res.sendFile('index.html', { root: join(__dirname, '../public/pages') });
+    //res.render('index', { title: 'Visual Studio Code!' });
 });
 
 /* GET Quick Start. */
 index.get('/quickstart', function(req, res, next) {
-  res.render('quickstart');
+    res.render('quickstart');
 });
 
 export default index;
