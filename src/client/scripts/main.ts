62c5325a62d3ba275e249cd7490610f98a6be778
matthew <lordmat@gmail.com>
2016-01-07 23:08:58 +0000

    Small clean up and showing a small square on the canvas

--- src/client/scripts/main.ts
+++ src/client/scripts/main.ts
@@ -1,8 +1,6 @@
 import * as PIXI from 'pixi.js';
 import {Test} from './test';
 
-console.log(PIXI);
-
 // Create the renderer
 var renderer = PIXI.autoDetectRenderer(512, 512);
 
@@ -13,7 +11,16 @@ canvas.appendChild(renderer.view);
 // Create a container object called the `stage`
 var stage = new PIXI.Container();
 
+
+let rectangle = new PIXI.Graphics();
+rectangle.lineStyle(4, 0xFF3300, 1);
+rectangle.beginFill(0x66CCFF);
+rectangle.drawRect(0, 0, 32, 32);
+rectangle.endFill();
+rectangle.x = 170;
+rectangle.y = 170;
+stage.addChild(rectangle);
+
 // Tell the `renderer` to `render` the `stage`
 renderer.render(stage);
 
-console.log(new Test().num);
