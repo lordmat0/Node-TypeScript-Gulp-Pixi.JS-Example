16322699f2d17952b50ba5ddd088495dbaf0dcf1
matthew <lordmat@gmail.com>
2016-01-31 12:07:03 +0000

    Added gulp-util to stop the build failing

--- src/client/scripts/physics/player-damage.physics.ts
+++ src/client/scripts/physics/player-damage.physics.ts
@@ -7,7 +7,7 @@ export class PlayerDamagePhysics {
 
         if (currentColour >= 0x000000 && currentColour < 0x00ff00) {
             damagePercentage = (currentColour - 0x003300) / (0x00ff00 - 0x003300) * (0.44444444444 - 0) + 0;
-        } else if (currentColour === 0x00ff00 || currentColour < 0xffffff) {
+        } else if (currentColour === 0x00ff00 || currentColour <= 0xffffff) {
             damagePercentage = (currentColour - 0x00ff00) / (0xffffff - 0x00ff00) * (1 - 0.44444444444) + 0.44444444444;
         }
 
